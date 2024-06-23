063a1aa1302f046caa4412126806748b1a2f7e50
Abir Gupta <142162873+abirxgpt@users.noreply.github.com>
2024-06-24 01:27:01 +0530

    Create rules.py

---
+++ Day 12/rules.py
@@ -0,0 +1,13 @@
+Rules = '''
+For Easy:
+1. You will get 10 chances.
+2. If you get "Too High!!!/Too Low!!!," the guess is more than 25 numbers away.
+3. If you get "High/Low!!," the guess is 10 to 25 numbers away.
+4. If you get "High/Low, Close!!!!," the guess is 5 to 10 numbers away.
+5. If you get "High/Low, So Close!!!!," the guess is within 5 numbers.
+
+For Hard:
+1. You will get 5 chances.
+2. If you get "High/Low!!," the guess is more than 10 numbers away.
+3. If you get "High/Low, Close!!!!," the guess is within 10 numbers.
+'''
