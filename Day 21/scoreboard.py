8488a3a6c6890fe431955b9a24af3864b89df900
Abir Gupta <142162873+abirxgpt@users.noreply.github.com>
2024-07-16 01:48:08 +0530

    Update scoreboard.py

--- Day 21/scoreboard.py
+++ Day 21/scoreboard.py
@@ -6,6 +6,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("high_score.txt") as hs:
+            self.highscore = int(hs.read())
         self.showscore()
 
     def increase(self):
@@ -17,7 +19,7 @@ class Scoreboard(Turtle):
         self.color("white")
         self.penup()
         self.goto(0, 270)
-        self.write(f"Score:{self.score}", False, align="center", font=SCORE_FONT)
+        self.write(f"Score:{self.score} | High Score:{self.highscore}", False, align="center", font=SCORE_FONT)
         self.hideturtle()
 
     def game_over(self):
@@ -25,3 +27,13 @@ class Scoreboard(Turtle):
         self.color("white")
         self.hideturtle()
         self.write(f"Oops! GAME OVER {chr(10)}Score: {self.score}{chr(10)}Press 'R' to Play Again", False, align="center", font=GAME_OVER_FONT)
+
+    def reset_game(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+            with open("high_score.txt", mode="w") as hs:
+                hs.write(str(self.highscore))
+        self.score = 0
+        self.reset()
+        self.showscore()
+
