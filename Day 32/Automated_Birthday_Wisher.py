5c43bc098488b15043157977efe7941b23f597af
Abir Gupta <142162873+abirxgpt@users.noreply.github.com>
2024-07-25 02:36:52 +0530

    Create Automated_Birthday_Wisher.py

---
+++ Day 32/Automated_Birthday_Wisher.py
@@ -0,0 +1,46 @@
+import smtplib
+import datetime as dt
+import random as r
+import pandas
+
+# Constants
+password = "qhyhovphvhlevagh"
+email = "fuxnanami@gmail.com"
+
+
+# Defining Send Function
+def send_mail():
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=email, password=password)
+        connection.sendmail(
+            from_addr=email,
+            to_addrs=people['email'],
+            msg=f"Subject:Happy Birthday!!\n\n{Completed_letter}"
+        )
+
+
+birthday_list = pandas.read_csv("birthdays.csv")
+birthday_dict = birthday_list.to_dict('records')
+
+# Current Date
+today = dt.datetime.now()
+
+# Checking if Birthday is Today
+for people in birthday_dict:
+    if today.month == people["month"]:
+        if today.day == people["day"]:
+
+            # After Checking Opening one random Letter
+            with open(f"./letter_templates/letter_{r.randint(1,10)}.txt") as letter:
+                Letter = letter.read()
+                Completed_letter = Letter.replace("[NAME]", people['name'])
+
+            # Sending the mail on the Lucky person's Email Address
+            send_mail()
+
+
+
+
+
+
